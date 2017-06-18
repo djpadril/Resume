32dc53e25ddbfcc1fb9cf6aabcd40196007829fd
djpadril <djpadril@me.com>
2017-06-18 17:48:41 +0200

    resume
    my first online resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,88 @@
+var bio = {
+  "name" : "Patrick Gerber",
+  "role" : "ICT Business Analyst",
+  "dateOfBirth": "26.11.1985",
+  "nationality": "Swiss",
+  "hometown": "Bern, Switzerland"
+  "hobbies": ["sports", "music", "friends", "cooking and dining", "traveling", "photography", "technical gadgets",]
+    "contacts" : {
+    "mobile" : "+41 78 884 54 04",
+    "email" : "patrick.gerber@me.com",
+    "twitter" : "djpadril",
+    "location" : "Bern",
+  },
+"welcomeMessage" : "Welcome to my resume!",
+"skills": [
+  "project management", "requirements engineering", "business analysisi", "empathetic working"
+],
+"bioPic" : "images/patrick.jpg"
+}
+
+var work = {
+  "jobs": [
+    {
+      "position": "ICT Business Analyst";
+      "employer": "Garaio AG";
+      "duration": = "2 years";
+      "city": "Bern, Switzerland"
+    },
+    {
+      "position": "Application Manager";
+      "employer": "SRG SSR ideé suisse";
+      "duration": "1 year";
+      "city": "Bern, Switzerland"
+    },
+    {
+      "position": "Application Specialist"
+      "employer": "Tamedia AG";
+      "duration": "6 years";
+      "city": "Bern, Switzerland"
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "project": "Post Home Button";
+      "description": "Building a IoT device to deliver new services to the customer with a phygital access point. Business analasys and requirements engineeringto develope the hard- and softwareto bring postal services and products to the customers home";
+      "duration": "ongoing"
+    },
+    {
+      "project": "Outsourcing Swisscom Online"
+      "description": "Development and Support of the existing software solution. Business analysis and requirements engineering";
+      "duration": "1.5 years";
+      "city": "Bern, Switzerland"
+    },
+    {
+      "project": "APM@SRG";
+      "description": "As part of my diploma I did the project setup and procurement of an application performance management solution to measure the customer experience of relevant Business Applications for my customer SRG SSR.";
+      "duration": "8 months"
+    }
+  ]
+  }
+
+var education = {
+	"schools": [
+    {
+			"name": "Höhere Fachschule für Wirtschaftsinformatik",
+			"city": "Bern, Switzerland",
+			"major": "[economics, business information systems]",
+			"years": "2012 - 2015"
+		},
+		{
+			"name": "Schule für Gestaltung Bern-Biel",
+			"city": "Bern, Switzerland",
+			"major": "[graphic design, media technology]",
+			"years": "2001 - 2005"
+		}
+	],
+  "online courses": [
+    {
+      "title": "Basic Programming",
+      "school": "Udacity",
+      "dates": "2017",
+      "url": "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+    }
+  ]
+}
