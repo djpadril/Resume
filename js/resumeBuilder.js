e0a35cdef527f07846de6f862305f48484f62e43
djpadril <djpadril@me.com>
2017-07-14 16:23:55 +0200

    modifications after review

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,14 +2,14 @@
     "name" : "Patrick Gerber",
     "role" : "ICT Business Analyst",
     "contacts": {
-    "adress": "Weingartstrasse 9, 3014 Bern",
-    "location": "Bern, Switzerland",
     "mobile": "+4178 884 54 04",
-    "email": "patrick.gerber@me.com"
+    "email": "patrick.gerber@me.com",
+    "github": "https://github.com/djpadril",
+    "location": "Bern, Switzerland",
   },
     "welcomeMessage": "Welcome to my resume",
     "skills": ["project management", "requirements engineering", "business analysisys", "empathetic working"],
-    "picture": "images/patrick.jpg"
+    "biopic": "images/patrick.jpg"
   };
 
 
@@ -21,29 +21,26 @@ bio.display=function() {
   var formattedName = HTMLheaderName.replace("%data%", bio.name);
   $("#header").prepend(formattedName);
 
-  var formattedAdress = HTMLcontactGeneric.replace("%data%", bio.contacts.adress);
-  $("#topContacts, #footerContacts").append(formattedAdress);
-
   var formattedLocation= HTMLlocation.replace("%data%", bio.contacts.location);
-  $("#topContacts, #footerContacts").append(formattedLocation);
-
   var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-  $("#topContacts, #footerContacts").append(formattedMobile);
-
   var formattedEMail = HTMLemail.replace("%data%", bio.contacts.email);
-  $("#topContacts, #footerContacts").append(formattedEMail);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
 
-  var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
+  //append all contacts to the html
+  $("#topContacts, #footerContacts").append(formattedLocation, formattedMobile, formattedGithub, formattedWelcomeMessage);
+
+  var formattedPicture = HTMLbioPic.replace("%data%", bio.biopic);
   $("#header").append(formattedPicture);
 
   var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-  $("#header").append(formattedWelcomeMessage);
+  $("#header").append();
 
   $("#header").append(HTMLskillsStart);
-  for (var skill = 0; skill < bio.skills.length; skill++) {
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+
+  bio.skills.forEach (function(skill) {
+    var formattedSkill = HTMLskills.replace("%data%", skill);
     $("#skills:last").append(formattedSkill);
-  }
+  })
 };
 
 bio.display();
@@ -53,7 +50,7 @@ var education = {
     {
 			"name": "Höhere Fachschule für Wirtschaftsinformatik",
       "degree": "Business Data Specialist, Advanced Federal Diploma of Higher Education",
-			"major": "economics, business information systems",
+			"majors": ["economics", " " + "business information systems"],
       "location": "Bern, Switzerland",
 			"dates": "2012 - 2015",
       "url": "https://www.ifa.ch/nc/weiterbildung/studiengaenge/kursdetail/dipl-wirtschaftsinformatikerin-hf/kurs/show/"
@@ -61,7 +58,7 @@ var education = {
 		{
 			"name": "Schule für Gestaltung Bern-Biel",
       "degree": "Polygraf EFZ",
-			"major": "graphic design, media technology",
+			"majors": ["graphic design", " " + "media technology"],
       "location": "Bern, Switzerland",
 			"dates": "2001 - 2005",
       "url": "http://www.sfgb-b.ch/sfgb/de/bildungsangebote/berufsfachschule/lehrberufe/polygraf.html"
@@ -78,42 +75,42 @@ var education = {
 };
 
 education.display=function() {
-  for (var school in education.schools) {
+  education.schools.forEach(function(school) {
       $("#education").append(HTMLschoolStart);
 
-      var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-      var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+      var formattedName = HTMLschoolName.replace("%data%", school.name);
+      var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
       var formattedEducationTitle = formattedName + formattedDegree;
       $(".education-entry:last").append(formattedEducationTitle);
 
-      var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+      var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
       $(".education-entry:last").append(formattedDates);
 
-      var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+      var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
       $(".education-entry:last").append(formattedLocation);
 
-      var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+      var formattedMajor = HTMLschoolMajor.replace("%data%", school.majors);
       $(".education-entry:last").append(formattedMajor);
 
-      var formattedUrl = HTMLonlineURL.replace("%data%", education.schools[school].url);
+      var formattedUrl = HTMLonlineURL.replace("%data%", school.url);
       $(".education-entry:last").append(formattedUrl);
-    }
+    });
 
       $("#education").append(HTMLonlineClasses);
 
-    for (var onlineCourse in education.onlineCourses) {
+    education.onlineCourses.forEach(function(onlineCourse) {
       $("#education").append(HTMLschoolStart);
-      var OnlineCourseName = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title);
-      var OnlineCourseSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school);
+      var OnlineCourseName = HTMLonlineTitle.replace("%data%", onlineCourse.title);
+      var OnlineCourseSchool = HTMLonlineSchool.replace("%data%", onlineCourse.school);
       var OnlineCourseTitle = OnlineCourseName + OnlineCourseSchool;
       $(".education-entry:last").append(OnlineCourseTitle);
 
-      var OnlineCourseDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates);
+      var OnlineCourseDates = HTMLonlineDates.replace("%data%", onlineCourse.dates);
       $(".education-entry:last").append(OnlineCourseDates);
 
-      var OnlineCourseUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url);
+      var OnlineCourseUrl = HTMLonlineURL.replace("%data%", onlineCourse.url);
       $(".education-entry:last").append(OnlineCourseUrl);
-  }
+  })
 };
 
 education.display();
@@ -124,23 +121,23 @@ education.display();
   var work = {
     "jobs": [
       {
-        "position": "ICT Business Analyst",
+        "title": "ICT Business Analyst",
         "employer": "Garaio AG",
-        "duration": "2015 - present",
+        "dates": "2015 - present",
         "location": "Bern and Zurich, Switzerland",
         "description": "Lorem Ipsum Doler ..."
       },
       {
-        "position": "Application Manager",
+        "title": "Application Manager",
         "employer": "SRG SSR ideé suisse",
-        "duration": "2014 - 2015",
+        "dates": "2014 - 2015",
         "location": "Bern, Switzerland",
         "description": "Lorem Ipsum Doler ..."
       },
       {
-        "position": "Application Specialist",
+        "title": "Application Specialist",
         "employer": "Tamedia AG",
-        "duration": "2008 - 2014",
+        "dates": "2008 - 2014",
         "location": "Bern and Zurich, Switzerland",
         "description": "Lorem Ipsum Doler ..."
       }
@@ -148,69 +145,71 @@ education.display();
   };
 
   work.display=function() {
-    for (var job in work.jobs) {
+    work.jobs.forEach(function(job) {
       // create new div for workExperience
       $("#workExperience").append(HTMLworkStart);
       // concat employer and job position
-      var formattedEmployer = HTMLworkEmployer.replace ("%data%", work.jobs[job].employer);
-      var formattedPosition = HTMLworkTitle.replace("%data%", work.jobs[job].position);
+      var formattedEmployer = HTMLworkEmployer.replace ("%data%", job.employer);
+      var formattedPosition = HTMLworkTitle.replace("%data%", job.title);
       var formattedEmployerTitle = formattedEmployer + formattedPosition;
       $(".work-entry:last").append(formattedEmployerTitle);
 
-      var formattedDates = HTMLworkDates.replace ("%data%", work.jobs[job].duration);
+      var formattedDates = HTMLworkDates.replace ("%data%", job.dates);
       $(".work-entry:last").append(formattedDates);
 
-      var formattedLocation = HTMLworkLocation.replace ("%data%", work.jobs[job].location);
+      var formattedLocation = HTMLworkLocation.replace ("%data%", job.location);
       $(".work-entry:last").append(formattedLocation);
 
-      var formattedDescription = HTMLworkDescription.replace ("%data%", work.jobs[job].description);
+      var formattedDescription = HTMLworkDescription.replace ("%data%", job.description);
       $(".work-entry:last").append(formattedDescription);
-    }
+    })
   };
 
   work.display();
 
 var projects = {
-    "project": [
+    "projects": [
       {
         "title": "Order Button",
         "description": "Building a IoT device to deliver new services to the customer with a phygital access point. Business analasys and requirements engineering to develope the hard- and software to bring postal services and products to the customers home",
-        "duration": "ongoing",
-        "image": "images/IoT.jpg"
+        "dates": "ongoing",
+        "images": ["images/IoT.jpg"]
       },
       {
         "title": "Outsourcing Swisscom Online",
         "description": "Development and Support of the existing software solution. Business analysis and requirements engineering",
-        "duration": "1.5 years",
-        "image": "images/eCommerce.jpg"
+        "dates": "1.5 years",
+        "images": ["images/eCommerce.jpg"]
       },
       {
         "title": "APM@SRG",
         "description": "As part of my diploma I did the project setup and procurement of an application performance management solution to measure the customer experience of relevant Business Applications for my customer SRG SSR.",
-        "duration": "8 months",
-        "image": "images/apm.jpg"
+        "dates": "8 months",
+        "images": ["images/apm.jpg"]
       }
     ]
   };
 
 projects.display=function() {
 
-      for (var project in projects.project) {
+      projects.projects.forEach(function(project) {
         // create new div for projectExperience
         $("#projects").append(HTMLprojectStart);
 
-        var formattedProjectTitle = HTMLprojectTitle.replace ("%data%", projects.project[project].title);
+        var formattedProjectTitle = HTMLprojectTitle.replace ("%data%", project.title);
         $(".project-entry:last").append(formattedProjectTitle);
 
-        var formattedProjectDuration = HTMLprojectDates.replace ("%data%", projects.project[project].duration);
+        var formattedProjectDuration = HTMLprojectDates.replace ("%data%", project.dates);
         $(".project-entry:last").append(formattedProjectDuration);
 
-        var formattedProjectDescription = HTMLprojectDescription.replace ("%data%", projects.project[project].description);
+        var formattedProjectDescription = HTMLprojectDescription.replace ("%data%", project.description);
         $(".project-entry:last").append(formattedProjectDescription);
 
-        var formattedProjectImage = HTMLprojectImage.replace ("%data%", projects.project[project].image);
+        project.images.forEach(function(image) {
+        var formattedProjectImage = HTMLprojectImage.replace ("%data%", project.images);
         $(".project-entry:last").append(formattedProjectImage);
-    }
+    });
+  });
   };
 
 projects.display();
